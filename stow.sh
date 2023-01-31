0e8e2ab525ef47fb6c71602e46431fa6a0f80c18
TheBluePineapple <TBP>
2023-01-31 18:26:14 -0500

    update stow.sh script

--- stow.sh
+++ stow.sh
@@ -2,5 +2,11 @@
 echo stowing dotfiles...
 stow */
 git add .
-git commit -m "$1"
+if [ -z "$1" ]
+  then
+    echo "no commit message supplied"
+    git commit
+  else
+    git commit -m "$1"
+fi
 git push -u origin
